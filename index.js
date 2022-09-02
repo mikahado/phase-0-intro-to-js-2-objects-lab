95e596ea0abebdd2b13c347cfefa699e9d843712
Michael <mikahado@gmail.com>
2022-09-02 10:23:34 -0400

    commit tree 8f8cc7565d970554aa30317d9586843a8745fa62

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+// Write your solution in this file!
+const employee = {
+    name: "Mike",
+    streetAddress: "77 Broadway",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+        const newEmployee = {...employee};
+        newEmployee[key] = value;
+    return newEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee
+
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee2 = {...employee[key]}
+    delete newEmployee2[key]
+    return newEmployee2
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+
+}
+
+    
